9a1289cf55a32489e5b68b035b1e0318236d0f54
PastrykKiszelewski <patryk.kiszelewski@kainos.com>
2022-07-07 09:39:29 +0200

    added: working connection to db throught api

--- app/controller/JobController.js
+++ app/controller/JobController.js
@@ -7,7 +7,7 @@ const JobService = require('../service/JobService.js')
 router.get('/job-roles', async (req, res) => {   
     try {
         data = await JobService.getJobRoles()
-        
+        console.log(data);
         for (let i = 0; i < data.length; i++) {
             data[i].role_name = `<td id = 'name${data[i].role_id}' class="govuk-table__cell">${data[i].role_name}</td>`
         }
--- app/service/JobService.js
+++ app/service/JobService.js
@@ -2,12 +2,13 @@ const axios = require('axios');
 const { response } = require('express');
 axios.defaults.baseURL = process.env.API_URL;
 
+URL = '/api'
 
 module.exports.getJobRoles = async function () {
     try{
-        const response = await axios.get('/job-roles')
+        const response = await axios.get('http://localhost:8080/api/job-roles')
+        return response.data
     }catch(e){
         throw new Error("Could not find job roles")
     }
-
 }
