a904f92fa62b786bd85935520061e3b64be925e8
Conor Gallagher <conor.gallagher@kainos.com>
2022-07-15 16:36:10 +0100

    Working frontend now, also changed some wording in other files for consistency e.g. the back buttons

--- app/controller/JobController.js
+++ app/controller/JobController.js
@@ -31,4 +31,19 @@ router.get('/band-comp/:id', async (req, res) => {
     }
 });
 
+//take request body and response body
+//await for data from api with sended role id, 
+//render jobRespView with data and role name if not error
+//if error occurs, render error message
+router.get('/job-responsibility/:id', async (req, res) => {
+    try {
+        data = await JobService.getJobResponsibilityByID(req.params.id) 
+        res.render('jobRespView', {responsibilities: data})
+    } catch (e) {
+        res.locals.errormassege = e
+        res.render('jobRespView')
+    }
+});
+
+
 module.exports = router
