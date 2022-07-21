4a6e669366d7015f8fa7723a10f4235941b0db89
orlagh.mccloskey <orlagh.mccloskey@kainos.com>
2022-07-21 09:08:28 +0100

    added correct number of capabilities

--- app/validator/AddJobRoleValidator.js
+++ app/validator/AddJobRoleValidator.js
@@ -1,6 +1,6 @@
 const dropDownData = {
     band: [1, 2, 3, 4, 5, 6, 7, 8],
-    capability : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    capability : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 }
 
 module.exports.validateUserInput = function (role) {
